4d35034d5461508fb28aab0c0ca45a8cb20d0732
deepak <deepak@mail>
2024-06-29 13:00:47 +0530

    new commit

--- provider.tf
+++ provider.tf
@@ -4,8 +4,8 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key              = var.AWS_ACCESS_KEY_ID
-  secret_key              = var.AWS_SECRET_ACCESS_KEY
+  access_key              = var.AWS_ACCESS_KEY
+  secret_key              = var.AWS_SECRET_KEY
   default_tags {
     tags = {
       hashicorp-learn = "refresh"
--- variables.tf
+++ variables.tf
@@ -61,12 +61,12 @@ terraform {
 }
 
 */
-variable "AWS_ACCESS_KEY_ID" {
+variable "AWS_ACCESS_KEY" {
   description = "The AWS access key ID"
   type        = string
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
+variable "AWS_SECRET_KEY" {
   description = "The AWS secret access key"
   type        = string
 }
